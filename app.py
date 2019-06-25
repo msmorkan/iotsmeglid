ea39f2c108094d6829bf2873df488fc2545648aa
msmorkan <46044776+msmorkan@users.noreply.github.com>
2019-06-25 14:47:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ class HttpWSSProtocol(websockets.WebSocketServerProtocol):
         if path == '/ws':
             # HACK: Put the read data back, to continue with normal WS handling.
             self.reader.feed_data(bytes(request_line))
+            self.reader.feed_data("\r\n")
             self.reader.feed_data(headers.as_bytes().replace(b'\n', b'\r\n'))
             
             return await super(HttpWSSProtocol, self).handler()
